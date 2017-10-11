c3f8819d1afab4d83ddff847d50f6efd9ef49e71
Aravelli <32703894+Aravelli@users.noreply.github.com>
2017-10-11 15:06:11 +0400

    Update and rename Siva.bat to Sharanya.bat

---
+++ HYBRID/Sharanya.bat
@@ -0,0 +1,2 @@
+cd D:\SeleniumProject\sharanyaSeleniumProject\SivaSelProject
+java -cp D:\SeleniumProject\sharanyaSeleniumProject\sharanyaSelProject\CommonJarFiles\*;D:\SeleniumProject\sharanyaSeleniumProject\sharanyaSelProject\bin org.testng.TestNG D:\SeleniumProject\SivaSeleniumProject\sharanyaSelProject\sharanya.xml
